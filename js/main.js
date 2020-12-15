f73c68fa5101f855c8ba015b2858fc273193223d
adam-sungmin-park <parksm2@uci.edu>
2020-12-15 23:07:09 +0000

    user can view animations

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var xhrFilms = new XMLHttpRequest();
+xhrFilms.open('GET', 'https://ghibliapi.herokuapp.com/films');
+xhrFilms.responseType = 'json';
+xhrFilms.addEventListener('load', function () {
+
+  for (var i = 0; i < xhrFilms.response.length; i++) {
+    var titleElement = document.createElement('div');
+    titleElement.setAttribute('class', 'titles');
+    var titles = xhrFilms.response[i].title;
+    titleElement.textContent = titles;
+    var imageName = titles.toLowerCase().replace(/\s/g, '');
+    var titleImage = document.createElement('img');
+    titleImage.setAttribute('src', 'images/' + imageName + '.jpeg');
+    var $animationTitle = document.querySelector('.animationTitle');
+    var titleImages = document.createElement('div');
+    titleImages.setAttribute('class', 'imageLocation');
+    titleImage.setAttribute('width', '100px');
+    titleImage.setAttribute('height', '100px');
+    titleImage.setAttribute('class', 'img');
+
+    $animationTitle.append(titleElement);
+    titleElement.append(titleImages);
+    titleImages.append(titleImage);
+  }
+});
+xhrFilms.send();
