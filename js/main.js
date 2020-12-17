e161a3695e2301df780e6be0e85d0b379142ebde
adam-sungmin-park <parksm2@uci.edu>
2020-12-17 00:09:56 +0000

    user can view details

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 var xhrFilms = new XMLHttpRequest();
 xhrFilms.open('GET', 'https://ghibliapi.herokuapp.com/films');
 xhrFilms.responseType = 'json';
+var $container = document.querySelector('.container');
+var $bigAnime = document.createElement('img');
+var $animationDetails = document.querySelector('.animationDetails');
+var $animationList = document.createElement('div');
+$animationList.setAttribute('class', 'list2');
+var $favorite = document.createElement('div');
+var $description = document.createElement('div');
+var $navBar = document.querySelector('.navBar');
+$description.setAttribute('class', 'description');
+$favorite.setAttribute('class', 'favorite');
+$favorite.textContent = 'Add to Favorite🤍';
+var count = 0;
 
-
-xhrFilms.addEventListener('load', function () {
+xhrFilms.addEventListener('load', function (event) {
 
   for (var i = 0; i < xhrFilms.response.length; i++) {
     var titleElement = document.createElement('div');
@@ -12,54 +23,80 @@ xhrFilms.addEventListener('load', function () {
     titleElement.textContent = titles;
     var imageName = titles.toLowerCase().replace(/\s/g, '');
     var titleImage = document.createElement('img');
-    titleImage.setAttribute('id',titles)
+    titleImage.setAttribute('id', titles);
     titleImage.setAttribute('src', 'images/' + imageName + '.jpeg');
     var $animationTitle = document.querySelector('.animationTitle');
     var titleImages = document.createElement('div');
     titleImages.setAttribute('class', 'imageLocation');
     titleImage.setAttribute('class', 'img');
-
     $animationTitle.append(titleElement);
     titleElement.append(titleImages);
     titleImages.append(titleImage);
-    console.log(xhrFilms.response);
   }
 });
 xhrFilms.send();
 
+window.addEventListener('click', function (event) {
 
-
-var $container = document.querySelector('.container');
-var $animationDetails = document.querySelector('.animationDetails');
-var $animationList = document.querySelector('.list2');
-
-
-
-window.addEventListener('click',function(){
-  var $navBar = document.querySelector('.navBar');
-  console.log(event.target);
-  $container.className = 'hidden';
-  $animationDetails.className = 'view';
-
-   if(event.target.className === "titles"){
+  if (event.target.className === 'titles') {
+    $container.className = 'hidden';
+    $animationDetails.className = 'animationDetails view';
     $animationList.textContent = event.target.textContent;
-     var $bigAnime = document.createElement('img');
-     $bigAnime.setAttribute('src', 'images/' + event.target.textContent.toLowerCase().replace(/\s/g, '')+ '.jpeg');
-     $bigAnime.setAttribute('class', 'bigImage');
-     $animationDetails.append($bigAnime);
+    $bigAnime.setAttribute('src', 'images/' + event.target.textContent.toLowerCase().replace(/\s/g, '') + '.jpeg');
+    $bigAnime.setAttribute('class', 'bigImage');
+    $animationDetails.append($animationList);
+    $animationDetails.append($bigAnime);
+    count = 0;
   }
-  if(event.target.className === "img"){
+  if (event.target.className === 'img') {
+    $container.className = 'hidden';
+    $animationDetails.className = 'animationDetails view';
     $animationList.textContent = event.target.id;
-    var $bigAnime = document.createElement('img');
     $bigAnime.setAttribute('src', event.target.src);
     $bigAnime.setAttribute('class', 'bigImage');
+    $animationDetails.append($animationList);
     $animationDetails.append($bigAnime);
+    count = 0;
   }
-  if (event.target.textContent === "Home") {
-    $container.className = "container";
-    $animationDetails.className = 'hidden';
-    var $previousImage = document.querySelector('.hidden');
-    $previousImage.lastElementChild.remove();
-  }
+});
 
-})
+window.addEventListener('click', function (event) {
+  for (var i = 0; i < xhrFilms.response.length; i++) {
+    if (event.target.className === 'titles' && event.target.textContent === xhrFilms.response[i].title) {
+      $description.textContent = xhrFilms.response[i].description;
+      $animationDetails.append($description);
+      $animationDetails.append($favorite);
+    }
+    if (event.target.className === 'img' && event.target.id === xhrFilms.response[i].title) {
+      $description.textContent = xhrFilms.response[i].description;
+      $animationDetails.append($description);
+      $animationDetails.append($favorite);
+    }
+  }
+});
+
+$navBar.addEventListener('click', function (event) {
+  if (event.target.textContent === 'Home') {
+    $container.className = 'container';
+    $animationDetails.className = 'animationDetails view';
+    var $previousImage = document.querySelector('body > div.animationDetails.view>img');
+    $previousImage.remove();
+    document.querySelector('body > div.animationDetails.view > div.list2').remove();
+    document.querySelector('body > div.animationDetails.view > div.description').remove();
+    document.querySelector('body > div.animationDetails.view > div.favorite').remove();
+  }
+});
+
+window.addEventListener('click', function (event) {
+
+  if (event.target.className === 'favorite') {
+    count++;
+    if (count % 2 === 0) {
+      alert('added to favorites!');
+      $favorite.textContent = '♥️';
+    }
+    if (count % 2 === 1) {
+      $favorite.textContent = 'Add to Favorite🤍';
+    }
+  }
+});
